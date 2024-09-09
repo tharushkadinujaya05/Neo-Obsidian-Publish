a448a6258771ebfb9ec17fadaeeb7e078eaa48e8
tharushkadinujaya <tharushkadinujaya05@gmail.com>
2024-09-09 20:47:01 +0530

    Quartz sync: Sep 9, 2024, 8:47 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -2,6 +2,7 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
 import { i18n } from "../i18n"
+import { footer } from "mdast-util-to-hast/lib/footer"
 
 interface Options {
   links: Record<string, string>
@@ -12,18 +13,8 @@ export default ((opts?: Options) => {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
-      <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
+      <footer>
+        <p>neospace©2024</p>
       </footer>
     )
   }
