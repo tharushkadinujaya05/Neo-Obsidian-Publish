bc44a4255a4b6d1c2d63c5383b21fcf23e057157
tharushkadinujaya <tharushkadinujaya05@gmail.com>
2024-09-09 18:24:03 +0530

    Quartz sync: Sep 9, 2024, 6:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🐉 Neo's Obsidian",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
